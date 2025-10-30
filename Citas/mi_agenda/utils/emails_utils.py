7302c88460c4746a9d295dcd8ba1a281916574a8
Lizeth Crisosto <142123394+Liz2910@users.noreply.github.com>
2025-10-29 21:06:44 -0600

    Refactor enviar_correo to improve error handling
    Refactor email sending function to include error handling and use mail configuration from the app.

--- Citas/mi_agenda/utils/emails_utils.py
+++ Citas/mi_agenda/utils/emails_utils.py
@@ -1,20 +1,20 @@
-from flask_mail import Mail, Message
-from flask import current_app
+from flask_mail import Message
 
-def enviar_correo(nombre, telefono, fecha, hora, nota):
-    mail = Mail(current_app)
-    msg = Message(
-        subject="Nueva cita agendada",
-        sender="lmcg2910@gmail.com",
-        recipients=["lmcg2910@gmail.com"],
-        body=f"""
-        Se ha agendado una nueva cita:
-        Nombre: {nombre}
-        Teléfono: {telefono}
-        Fecha: {fecha}
-        Hora: {hora}
-        Nota: {nota}
-        """
-    )
-
-    mail.send(msg)
+def enviar_correo(mail, nombre, telefono, fecha, hora, nota):
+    try:
+        msg = Message(
+            subject="Nueva cita agendada",
+            recipients=[mail.app.config["MAIL_USERNAME"]],
+            body=f"""
+            Se ha agendado una nueva cita:
+            Nombre: {nombre}
+            Teléfono: {telefono}
+            Fecha: {fecha}
+            Hora: {hora}
+            Nota: {nota}
+            """
+        )
+        mail.send(msg)
+        print("✅ Correo enviado correctamente")
+    except Exception as e:
+        print("⚠️ Error al enviar correo:", e)
